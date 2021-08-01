6a6807ec96d3b2b56ce7c0857b91f40e1569d15b
ericolivrib <ribericoliveira@protonmail.ch>
2021-08-01 20:55:16 -0300

    Laço de repetição para criar o ônibus.

--- src/model/Onibus.java
+++ src/model/Onibus.java
@@ -4,103 +4,15 @@ import java.util.ArrayList;
 
 public class Onibus {
 
-    private final ArrayList<Poltrona> poltronas = new ArrayList<>(28);
+    private final int LUGARES = 46;
+    private final ArrayList<Poltrona> poltronas = new ArrayList<>(LUGARES);
 
     public Onibus() {
 
-        Poltrona poltrona1 = new Poltrona(1, true);
-        Poltrona poltrona2 = new Poltrona(2, true);
-        Poltrona poltrona3 = new Poltrona(3, true);
-        Poltrona poltrona4 = new Poltrona(4, true);
-        Poltrona poltrona5 = new Poltrona(5, true);
-        Poltrona poltrona6 = new Poltrona(6, true);
-        Poltrona poltrona7 = new Poltrona(7, true);
-        Poltrona poltrona8 = new Poltrona(8, true);
-        Poltrona poltrona9 = new Poltrona(9, true);
-        Poltrona poltrona10 = new Poltrona(10, true);
-        Poltrona poltrona11 = new Poltrona(11, true);
-        Poltrona poltrona12 = new Poltrona(12, true);
-        Poltrona poltrona13 = new Poltrona(13, true);
-        Poltrona poltrona14 = new Poltrona(14, true);
-        Poltrona poltrona15 = new Poltrona(15, true);
-        Poltrona poltrona16 = new Poltrona(16, true);
-        Poltrona poltrona17 = new Poltrona(17, true);
-        Poltrona poltrona18 = new Poltrona(18, true);
-        Poltrona poltrona19 = new Poltrona(19, true);
-        Poltrona poltrona20 = new Poltrona(20, true);
-        Poltrona poltrona21 = new Poltrona(21, true);
-        Poltrona poltrona22 = new Poltrona(22, true);
-        Poltrona poltrona23 = new Poltrona(23, true);
-        Poltrona poltrona24 = new Poltrona(24, true);
-        Poltrona poltrona25 = new Poltrona(25, true);
-        Poltrona poltrona26 = new Poltrona(26, true);
-        Poltrona poltrona27 = new Poltrona(27, true);
-        Poltrona poltrona28 = new Poltrona(28, true);
-        Poltrona poltrona29 = new Poltrona(29, true);
-        Poltrona poltrona30 = new Poltrona(30, true);
-        Poltrona poltrona31 = new Poltrona(31, true);
-        Poltrona poltrona32 = new Poltrona(32, true);
-        Poltrona poltrona33 = new Poltrona(33, true);
-        Poltrona poltrona34 = new Poltrona(34, true);
-        Poltrona poltrona35 = new Poltrona(35, true);
-        Poltrona poltrona36 = new Poltrona(36, true);
-        Poltrona poltrona37 = new Poltrona(37, true);
-        Poltrona poltrona38 = new Poltrona(38, true);
-        Poltrona poltrona39 = new Poltrona(39, true);
-        Poltrona poltrona40 = new Poltrona(40, true);
-        Poltrona poltrona41 = new Poltrona(41, true);
-        Poltrona poltrona42 = new Poltrona(42, true);
-        Poltrona poltrona43 = new Poltrona(43, true);
-        Poltrona poltrona44 = new Poltrona(44, true);
-        Poltrona poltrona45 = new Poltrona(45, true);
-        Poltrona poltrona46 = new Poltrona(46, true);
-
-        poltronas.add(poltrona1);
-        poltronas.add(poltrona2);
-        poltronas.add(poltrona3);
-        poltronas.add(poltrona4);
-        poltronas.add(poltrona5);
-        poltronas.add(poltrona6);
-        poltronas.add(poltrona7);
-        poltronas.add(poltrona8);
-        poltronas.add(poltrona9);
-        poltronas.add(poltrona10);
-        poltronas.add(poltrona11);
-        poltronas.add(poltrona12);
-        poltronas.add(poltrona13);
-        poltronas.add(poltrona14);
-        poltronas.add(poltrona15);
-        poltronas.add(poltrona16);
-        poltronas.add(poltrona17);
-        poltronas.add(poltrona18);
-        poltronas.add(poltrona19);
-        poltronas.add(poltrona20);
-        poltronas.add(poltrona21);
-        poltronas.add(poltrona22);
-        poltronas.add(poltrona23);
-        poltronas.add(poltrona24);
-        poltronas.add(poltrona25);
-        poltronas.add(poltrona26);
-        poltronas.add(poltrona27);
-        poltronas.add(poltrona28);
-        poltronas.add(poltrona29);
-        poltronas.add(poltrona30);
-        poltronas.add(poltrona31);
-        poltronas.add(poltrona32);
-        poltronas.add(poltrona33);
-        poltronas.add(poltrona34);
-        poltronas.add(poltrona35);
-        poltronas.add(poltrona36);
-        poltronas.add(poltrona37);
-        poltronas.add(poltrona38);
-        poltronas.add(poltrona39);
-        poltronas.add(poltrona40);
-        poltronas.add(poltrona41);
-        poltronas.add(poltrona42);
-        poltronas.add(poltrona43);
-        poltronas.add(poltrona44);
-        poltronas.add(poltrona45);
-        poltronas.add(poltrona46);
+        for (int i = 1; i <= LUGARES; i++) {
+            Poltrona p = new Poltrona(i, true);
+            poltronas.add(p);
+        }
     }
 
     public ArrayList<Poltrona> getPoltronas() {
